675e4f994095327bc8f021cb194318ec4cc6c404
Rico <teh_programerer@interactivelaboratories.com>
2013-10-04 12:11:19 +0100

    |BUILD CONFIGURATION| [DOS] * Added a simple batch script to make the program (not using GNU Make yet,   because there are a few things which aren't working)
    |SOURCE]
    [DOS::Main]
    * Added line drawing
    * A white line is drawn against a blue background

--- Source/DOS/Source/Main.c
+++ Source/DOS/Source/Main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
 #define VIDEO_INT			0x10
 #define WRITE_PIXEL			0x0C
@@ -14,8 +15,16 @@
 #define SCREEN_HEIGHT	200
 #define NUM_COLOURS		256
 
+#define Sign( N ) ( ( N < 0 ) ? -1 :( ( N > 0 ) ? 1: 0 ) )
+
 typedef unsigned char	BYTE;
 typedef unsigned short	WORD;
+
+typedef struct __POINT
+{
+	int X;
+	int Y;
+}POINT;
 
 BYTE *VGA = ( BYTE * )0xA0000;
 WORD *Clock = ( WORD * )0x046C;
@@ -40,11 +49,65 @@ void PlotPixelDirect( const int p_Position, const BYTE p_Colour )
 	VGA[ p_Position ] = p_Colour;
 }
 
+void DrawLine( const POINT p_Point1, const POINT p_Point2, BYTE p_Colour )
+{
+	int i, dX, dY, sdX, sdY, dXAbs, dYAbs, X, Y, PlotX, PlotY;
+
+	dX = p_Point2.X - p_Point1.X;
+	dY = p_Point2.Y - p_Point1.Y;
+	dXAbs = abs( dX );
+	dYAbs = abs( dY );
+	sdX = Sign( dX );
+	sdY = Sign( dY );
+	X = dXAbs >> 1;
+	Y = dYAbs >> 1;
+	PlotX = p_Point1.X;
+	PlotY = p_Point1.Y;
+
+	VGA[ ( PlotY << 8 )+( PlotY << 6 ) + PlotX ] = p_Colour;
+
+	if( dXAbs >= dYAbs )
+	{
+		for( i = 0; i < dXAbs; ++i )
+		{
+			Y += dYAbs;
+			if( Y >= dXAbs )
+			{
+				Y -= dXAbs;
+				PlotY += sdY;
+			}
+
+			PlotX += sdX;
+
+			PlotPixel( PlotX, PlotY, p_Colour );
+		}
+	}
+	else
+	{
+		for( i = 0; i < dYAbs; ++i )
+		{
+			X += dXAbs;
+			if( X >= dYAbs )
+			{
+				X -= dYAbs;
+				PlotX += sdX;
+			}
+
+			PlotY += sdY;
+
+			PlotPixel( PlotX, PlotY, p_Colour );
+		}
+	}
+}
+
 int main( int p_Argc, char **p_ppArgv )
 {
 	const int Colour = 1;
+	int LineColour;
+	int Counter = 0;
 	float T1;
 	WORD i, Start;
+	POINT P0, P1;
 
 	if( __djgpp_nearptr_enable( ) == 0 )
 	{
@@ -55,22 +118,33 @@ int main( int p_Argc, char **p_ppArgv )
 
 	VGA += __djgpp_conventional_base;
 	Clock = ( void * )Clock + __djgpp_conventional_base;
+	srand( *Clock );
+	P0.X = 0;
+	P0.Y = 0;
+	P1.X = 200;
+	P1.Y = 200;
+	LineColour = 15;
 
 	SetVideoMode( VGA_256_COLOUR_MODE );
 
 	Start = *Clock;
-	for( i = 0; i < ( 320*200 ); ++i )
+
+	while( T1 < 1.0f )
 	{
-		PlotPixelDirect( i, Colour );
+		for( i = 0; i < ( 320*200 ); ++i )
+		{
+			PlotPixelDirect( i, Colour );
+		}
+
+		DrawLine( P0, P1, LineColour );
+
+		T1 = ( *Clock - Start )/18.2f;
+		++Counter;
 	}
-
-	T1 = ( *Clock - Start )/18.2f;
-
-	sleep( 2 );
 
 	SetVideoMode( TEXT_MODE );
 
-	printf( "Took %f seconds to clear screen\n", T1 );
+	printf( "Rendered %d frames in one second\n", Counter );
 
 	__djgpp_nearptr_disable( );
 
