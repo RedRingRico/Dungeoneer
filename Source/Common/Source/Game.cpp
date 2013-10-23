dd04c81a57bdf77fd60db8a0a601f6f433c52f9f
Rico <teh_programerer@interactivelaboratories.com>
2013-10-23 11:53:38 +0100

    |SOURCE| [Common::Game] * A camera is used to calculate the projection and view matrices * W, A, S, D keys used for moving forward, left, backward, and right

--- Source/Common/Source/Game.cpp
+++ Source/Common/Source/Game.cpp
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <GitVersion.hpp>
 #include <Utility/Grid.hpp>
+#include <Renderer/Camera.hpp>
+
+const ZED_FLOAT32 STRAFE_SPEED = 0.2f;
+const ZED_FLOAT32 THRUST_SPEED = 0.2f;
 
 namespace Dungeoneer
 {
@@ -71,14 +75,23 @@ namespace Dungeoneer
 		GridColour.Blue = 0.0f;
 		GridColour.Alpha = 1.0f;
 
-		if( TestGrid.Initialise( 10, 10, ZED::Utility::PLANE_AXIS_XY,
+		if( TestGrid.Initialise( 10, 10, ZED::Utility::PLANE_AXIS_XZ,
 			GridColour, 0.0f, 0.1f ) != ZED_OK )
 		{
 			zedTrace( "Failed to create a grid\n" );
 			return ZED_FAIL;
 		}
 
-		ZED::Arithmetic::Matrix4x4 ProjView;
+		ZED::Renderer::Camera		MainCam;
+		ZED::Arithmetic::Vector3	CamPosition( 0.0f, 10.0f, 12.0f ),
+									CamLook( 0.0f, 0.0f, 0.0f ),
+									WorldUp( 0.0f, 1.0f, 0.0f );
+		ZED::Arithmetic::Matrix4x4	Projection, View, ProjView;
+
+
+		MainCam.ClippingPlanes( 1.0f, 10000.0f );
+		MainCam.PerspectiveProjection( 45.0f, 1280.0f / 720.0f, &Projection );
+		ZED::Arithmetic::Vector3 Velocity( 0.0f, 0.0f, 0.0f );
 
 		while( m_Running )
 		{
@@ -92,10 +105,36 @@ namespace Dungeoneer
 				XNextEvent( WindowData.pX11Display, &Event );
 			}
 
+			Velocity.Set( 0.0f, 0.0f, 0.0f );
+
+			if( m_Keyboard.IsKeyDown( 'a' ) )
+			{
+				Velocity[ 0 ] = -STRAFE_SPEED;
+			}
+			if( m_Keyboard.IsKeyDown( 'd' ) )
+			{
+				Velocity[ 0 ] = STRAFE_SPEED;
+			}
+			if( m_Keyboard.IsKeyDown( 'w' ) )
+			{
+				Velocity[ 2 ] = -THRUST_SPEED;
+			}
+			if( m_Keyboard.IsKeyDown( 's' ) )
+			{
+				Velocity[ 2 ] = THRUST_SPEED;
+			}
+
 			if( m_Keyboard.IsKeyDown( K_ESCAPE ) )
 			{
 				m_Running = ZED_FALSE;
 			}
+
+			CamPosition += Velocity;
+
+			MainCam.ViewLookAt( CamPosition, CamLook, WorldUp );
+			MainCam.View( &View );
+
+			ProjView = Projection * View;
 			
 			this->Update( 16667ULL );
 //			this->Render( );
