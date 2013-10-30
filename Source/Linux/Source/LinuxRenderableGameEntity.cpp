d8a8555c9a8160ac7b4e3ef6ed925f34182d4073
Rico <teh_programerer@interactivelaboratories.com>
2013-10-30 16:56:40 +0000

    |SOURCE| [Linux::RenderableGameEntity] * Added debug messages for errors and information when loading models and   shaders

--- Source/Linux/Source/LinuxRenderableGameEntity.cpp
+++ Source/Linux/Source/LinuxRenderableGameEntity.cpp
@@ -3,6 +3,7 @@
 #include <Renderer/OGL/GLModel.hpp>
 #include <System/Debugger.hpp>
 #include <Renderer/OGL/GLShader.hpp>
+#include <System/File.hpp>
 
 namespace Dungeoneer
 {
@@ -17,8 +18,19 @@ namespace Dungeoneer
 			return ZED_FAIL;
 		}
 
+		ZED_CHAR8 *pFile = new ZED_CHAR8[ ZED_MAX_PATH ];
+		if( p_Relative )
+		{
+			ZED::System::GetExecutablePath( &pFile, ZED_MAX_PATH );
+			strcat( pFile, p_pFile );
+		}
+		else
+		{
+			strcat( pFile, p_pFile );
+		}
+
 		zedTrace( "[Dungoneer::RenderableGameEntity::LoadModel] <INFO> "
-			"Loading model: \"%s\"\n", p_pFile );
+			"Loading model: \"%s\"\n", pFile );
 
 		switch( m_pRenderer->BackEnd( ) )
 		{
@@ -29,6 +41,9 @@ namespace Dungeoneer
 			}
 			default:
 			{
+				zedTrace( "[Dungeoneer::RenderableGameEntity::LoadModel] "
+					"<ERROR> Unknown renderer back end\n" );
+
 				return ZED_FAIL;
 			}
 		}
@@ -38,15 +53,23 @@ namespace Dungeoneer
 			zedTrace( "[Dungoneer::RenderableGameEntity::LoadModel] <ERROR> "
 				"Could not instantiate a model class for model: %s\n",
 				p_pFile );
+
 			return ZED_FAIL;
 		}
 
-		zedTrace( "Loading model %s...\n", p_pFile );
-		if( m_pModel->Load( p_pFile ) != ZED_OK )
+		if( m_pModel->Load( pFile ) != ZED_OK )
 		{
-			zedTrace( "Failed to load model\n" );
+			zedTrace( "[Dungeoneer::RenderableGameEntity::LoadModel] <ERROR> "
+				"Failed to load model\n" );
+
+			delete [ ] pFile;
+			pFile = ZED_NULL;
+
 			return ZED_FALSE;
 		}
+
+		delete [ ] pFile;
+		pFile = ZED_NULL;
 
 		return ZED_OK;
 	}
@@ -54,44 +77,76 @@ namespace Dungeoneer
 	ZED_UINT32 RenderableGameEntity::LoadShader( const ZED_CHAR8 *p_pFile,
 		const ZED_SHADER_TYPE p_Type, const ZED_BOOL p_Relative )
 	{
-		zedTrace( "Loading shader...\n" );
-
 		if( m_pRenderer == ZED_NULL )
 		{
+			zedTrace( "[Dungeoneer::RenderableGameEntity::LoadShader] <ERROR> "
+				"Renderer not set\n" );
+
 			return ZED_FAIL;
 		}
 
-		if( m_pShader )
+		if( ( ZED_MAX_PATH - strlen( p_pFile ) ) < 0 )
 		{
-			zedTrace( "Compiling shader...\n" );
-			m_pShader->Compile( &p_pFile, p_Type, ZED_TRUE );
+			zedTrace( "[Dungeoneer::RenderableGameEntity::LoadShader] <ERROR> "
+				"File path too long, exceeded by %d characters\n",
+				-( ZED_MAX_PATH - strlen( p_pFile ) ) );
 
-			return ZED_OK;
+			return ZED_FAIL;
 		}
 
-		switch( m_pRenderer->BackEnd( ) )
+		if( m_pShader == ZED_NULL )
 		{
-			case ZED_RENDERER_BACKEND_OPENGL:
+			switch( m_pRenderer->BackEnd( ) )
 			{
-				if( m_pRenderer->ShaderSupport( ) )
+				case ZED_RENDERER_BACKEND_OPENGL:
 				{
-					m_pShader = new ZED::Renderer::GLShader( );
-					zedTrace( "Compiling shader...\n" );
-					m_pShader->Compile( &p_pFile, p_Type, ZED_TRUE );
+					if( m_pRenderer->ShaderSupport( ) )
+					{
+						m_pShader = new ZED::Renderer::GLShader( );
 
-					return ZED_OK;
+						return ZED_OK;
+					}
+					else
+					{
+						return ZED_FAIL;
+					}
 				}
-				else
+				default:
 				{
+					zedTrace( "[Dungeoneer::RenderableGameEntity::LoadModel] "
+						"<ERROR> Unknown renderer back end\n" );
+
 					return ZED_FAIL;
 				}
 			}
-			default:
-			{
-				return ZED_FAIL;
-			}
 		}
-		
+		ZED_CHAR8 *pFile = new ZED_CHAR8[ ZED_MAX_PATH ];
+
+		if( p_Relative )
+		{
+			ZED::System::GetExecutablePath( &pFile, ZED_MAX_PATH );
+			strcat( pFile, p_pFile );
+		}
+		else
+		{
+			strcat( pFile, p_pFile );
+		}
+
+		zedTrace( "[Dungeoneer::RenderableGameEntity::LoadShader] <INFO> "
+			"Compliing shader: \"%s\"\n", p_pFile );
+
+		if( m_pShader->Compile( const_cast< const ZED_CHAR8 ** >( &pFile ),
+			p_Type, ZED_TRUE ) != ZED_OK )
+		{
+			delete [ ] pFile;
+			pFile = ZED_NULL;
+
+			return ZED_FAIL;
+		}
+
+		delete [ ] pFile;
+		pFile = ZED_NULL;
+
 		return ZED_OK;
 	}
 }
